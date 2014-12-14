44182a28b1ff605824252ec9d51af3f2dbef3b29
Kartik Khare <kharekartik@gmail.com>
2014-12-14 18:10:02 +0530

    Updated run-demo.py
    Demo now working for a single source.Improvements going on.

--- run-demo/run-demo.py
+++ run-demo/run-demo.py
@@ -1,28 +1,38 @@
 import sys
 import os
+import time
+from subprocess import call
 
 
 install_dir = os.getcwd()
+print install_dir+'/python-api'
+
 sys.path.insert(0, install_dir+'/python-api')
 
 from gstswitch.helpers import PreviewSinks
 from gstswitch.server import Server
 from gstswitch.helpers import TestSources
 
-#path = os.getcwd()
-#print path
+path = os.getcwd()
+
+print path
 
 #sys.path.insert(0, install_dir + 'gst-switch/python-api/gstswitch')
 
-PATH = '/usr/bin/'
+PATH = install_dir + '/tools/'
+sys.path.insert(0, install_dir+ '/tools')
+print PATH
+
 # The default location is '/usr/bin'. Change to wherever the gst-switch executables are located
 serv = Server(path=PATH, video_port=3000, audio_port=4000)
 serv.run()
 
+time.sleep(5)
+
 sources = TestSources(video_port=3000, audio_port=4000)
-sources.new_test_video()
-sources.new_test_audio()
+sources.new_test_video(pattern=1)
+call(["./tools/gst-switch-ui"])
 
-
-preview = PreviewSinks(preview_port=3001)
-preview.run()
+time.sleep(5)
+   
+sources.new_test_video(pattern=18)
